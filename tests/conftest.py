bf3d3991d15985b8d81bd61d241413800369e09d
Marco Treglia <mtreglia@gopro.com>
2023-03-14 16:51:49 +0100

    tests: update according the new architecture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,22 @@
+import atexit
+from pathlib import Path
+import shutil
+import uuid
+
 import pytest
 
 
-@pytest.fixture
+@pytest.fixture()
 def tmp_dir(request):
     """
-    Return a :class:`py.path.local` object pointing to the "tmp" directory
+    Return a :class:`pathlib.Path` object pointing to the "tmp" directory
     relative to the test file.
     """
-    tmp_dir = request.fspath.dirpath('tmp')
-    if tmp_dir.isdir():
-        tmp_dir.remove()
+    tmp_dir = Path(request.fspath.dirpath(f"tmp-{uuid.uuid4()}"))
     tmp_dir.mkdir()
-    return tmp_dir
+    yield tmp_dir
+
+    # remove temporary directory
+    if tmp_dir.exists():
+        # Windows compatible solution to clean-up the tmp_dir
+        atexit.register(shutil.rmtree, tmp_dir, ignore_errors=True)
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,31 +1,38 @@
-import os.path as op
+from pathlib import Path
+import platform
 import shutil
 
 import pytest
 
+import cython_setuptools
 
-this_dir = op.dirname(__file__)
-
-
-@pytest.mark.slow
-def test_compile_and_run_no_cythonize_mode(virtualenv, tmp_dir):
-    setup_path, pypkg_dir = _setup_source('setup-no-cythonize.py', tmp_dir)
-    virtualenv.run('CYTHONIZE=1 pip install -e %s' % pypkg_dir)
-    assert virtualenv.run('python -m bar', capture=True) == '2\n'
+this_dir = Path(__file__).parent
+cython_setuptools_path = (Path(cython_setuptools.__file__).parent).parent
 
 
-@pytest.mark.slow
-def test_compile_and_run_cythonize_mode(virtualenv, tmp_dir):
-    setup_path, pypkg_dir = _setup_source('setup-cythonize.py', tmp_dir)
-    virtualenv.run('pip install -e %s' % pypkg_dir)
-    assert virtualenv.run('python -m bar', capture=True) == '2\n'
-
-
-def _setup_source(setup_name, tmp_dir):
-    pypkg_dir = tmp_dir.join('pypkg')
-    src_dir = tmp_dir.join('src')
-    setup_path = pypkg_dir.join('setup.py')
-    shutil.copytree(op.join(this_dir, 'pypkg'), str(pypkg_dir))
-    shutil.copytree(op.join(this_dir, 'src'), str(src_dir))
-    setup_path.mksymlinkto(pypkg_dir.join(setup_name))
+def _setup_source(setup_name, tmp_dir: Path):
+    pypkg_dir = tmp_dir / "pypkg"
+    src_dir = tmp_dir / "src"
+    setup_path = pypkg_dir / "setup.py"
+    shutil.copytree(this_dir / "pypkg", str(pypkg_dir))
+    shutil.copytree(this_dir / "src", str(src_dir))
+    setup_path.symlink_to(pypkg_dir / setup_name)
     return setup_path, pypkg_dir
+
+
+@pytest.mark.skipif(
+    platform.system().lower() == "windows",
+    reason="Windows environment variable are not easy to manage",
+)
+def test_compile_and_run_external_cythonize_mode(virtualenv, tmp_dir):
+    _, pypkg_dir = _setup_source("setup-no-cythonize.py", tmp_dir)
+    virtualenv.run(f"pip install {cython_setuptools_path}")
+    virtualenv.run(f"export CYTHONIZE=1; pip install -e {pypkg_dir.resolve()}")
+    assert int(virtualenv.run("python -m bar", capture=True)) == 2
+
+
+def test_compile_and_run_cythonize_mode(virtualenv, tmp_dir):
+    _, pypkg_dir = _setup_source("setup-cythonize.py", tmp_dir)
+    virtualenv.run(f"pip install {cython_setuptools_path}")
+    virtualenv.run(f"pip install -e {pypkg_dir.resolve()}")
+    assert int(virtualenv.run("python -m bar", capture=True)) == 2
