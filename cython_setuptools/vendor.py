778d859a6b4f1e1b8bf9211d665fbca48a4c70df
Marco Treglia <mtreglia@gopro.com>
2023-03-14 16:29:54 +0100

    vendor: configparser deprecation resolved

--- cython_setuptools/vendor.py
+++ cython_setuptools/vendor.py
@@ -226,8 +226,8 @@ def parse_setup_cfg(fp, cythonize=False, pkg_config=None, base_dir=''):
     """
     if pkg_config is None:
         pkg_config = _run_pkg_config
-    config = configparser.SafeConfigParser()
-    config.readfp(fp)
+    config = configparser.ConfigParser()
+    config.read_file(fp)
     return _expand_cython_modules(config, cythonize, pkg_config, base_dir)
 
 
