647194580d84500e984b1b1c1fd48df9c1fbef9c
Marco Treglia <mtreglia@gopro.com>
2023-03-22 14:09:54 +0100

    tests: fix functions order

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -9,6 +9,16 @@ this_dir = op.dirname(__file__)
 cython_setuptools_path = op.abspath(op.join(cython_setuptools.__file__, "..", ".."))
 
 
+def _setup_source(setup_name, tmp_dir):
+    pypkg_dir = tmp_dir.join('pypkg')
+    src_dir = tmp_dir.join('src')
+    setup_path = pypkg_dir.join('setup.py')
+    shutil.copytree(op.join(this_dir, 'pypkg'), str(pypkg_dir))
+    shutil.copytree(op.join(this_dir, 'src'), str(src_dir))
+    setup_path.mksymlinkto(pypkg_dir.join(setup_name))
+    return setup_path, pypkg_dir
+
+
 @pytest.mark.slow
 def test_compile_and_run_no_cythonize_mode(virtualenv, tmp_dir):
     setup_path, pypkg_dir = _setup_source('setup-no-cythonize.py', tmp_dir)
@@ -23,13 +33,3 @@ def test_compile_and_run_cythonize_mode(virtualenv, tmp_dir):
     virtualenv.run(f"pip install {cython_setuptools_path}")
     virtualenv.run('pip install -e %s' % pypkg_dir)
     assert virtualenv.run('python -m bar', capture=True) == '2\n'
-
-
-def _setup_source(setup_name, tmp_dir):
-    pypkg_dir = tmp_dir.join('pypkg')
-    src_dir = tmp_dir.join('src')
-    setup_path = pypkg_dir.join('setup.py')
-    shutil.copytree(op.join(this_dir, 'pypkg'), str(pypkg_dir))
-    shutil.copytree(op.join(this_dir, 'src'), str(src_dir))
-    setup_path.mksymlinkto(pypkg_dir.join(setup_name))
-    return setup_path, pypkg_dir
