318c8ff7b7c487f2da073e90fd973f16805e53cc
Marco Treglia <mtreglia@gopro.com>
2023-03-22 15:51:51 +0100

    tests: use default pytest tmp_path fixture
    Use default tmp_path fixture. Return a temporary directory path object
    which is unique to each test function invocation, created as a sub
    directory of the base temporary directory.
    
    https://docs.pytest.org/en/7.1.x/reference/reference.html#pytest.tmpdir.tmp_path

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -9,25 +9,25 @@ this_dir = op.dirname(__file__)
 cython_setuptools_path = op.abspath(op.join(cython_setuptools.__file__, "..", ".."))
 
 
-def _setup_source(setup_name, tmp_dir):
-    pypkg_dir = tmp_dir.join('pypkg')
-    src_dir = tmp_dir.join('src')
-    setup_path = pypkg_dir.join('setup.py')
+def _setup_source(setup_name, tmp_path):
+    pypkg_dir = tmp_path / 'pypkg'
+    src_dir = tmp_path / 'src'
+    setup_path = pypkg_dir / 'setup.py'
     shutil.copytree(op.join(this_dir, 'pypkg'), str(pypkg_dir))
     shutil.copytree(op.join(this_dir, 'src'), str(src_dir))
-    os.symlink(pypkg_dir.join(setup_name), setup_path)
+    os.symlink(pypkg_dir / setup_name, setup_path)
     return setup_path, pypkg_dir
 
 
-def test_compile_and_run_no_cythonize_mode(virtualenv, tmp_dir):
-    setup_path, pypkg_dir = _setup_source('setup-no-cythonize.py', tmp_dir)
+def test_compile_and_run_no_cythonize_mode(virtualenv, tmp_path):
+    setup_path, pypkg_dir = _setup_source('setup-no-cythonize.py', tmp_path)
     virtualenv.run(f"pip install {cython_setuptools_path}")
     virtualenv.run(f"export CYTHONIZE=1; pip install -e {pypkg_dir}")
     assert virtualenv.run('python -m bar', capture=True) == '2\n'
 
 
-def test_compile_and_run_cythonize_mode(virtualenv, tmp_dir):
-    setup_path, pypkg_dir = _setup_source('setup-cythonize.py', tmp_dir)
+def test_compile_and_run_cythonize_mode(virtualenv, tmp_path):
+    setup_path, pypkg_dir = _setup_source('setup-cythonize.py', tmp_path)
     virtualenv.run(f"pip install {cython_setuptools_path}")
     virtualenv.run(f"pip install -e {pypkg_dir}")
     assert virtualenv.run('python -m bar', capture=True) == '2\n'
